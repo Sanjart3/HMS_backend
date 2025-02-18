88db92b37d519473735cbe6982f9d29025fe1de1
Sanjar Totliboyev <sanjartotliboyev@gmail.com>
2025-02-18 13:19:01 +0500

    Changes done for Patient Endpoints

--- src/main/java/org/tsa/hms_backend/services/PatientService.java
+++ src/main/java/org/tsa/hms_backend/services/PatientService.java
@@ -3,9 +3,13 @@ package org.tsa.hms_backend.services;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.tsa.hms_backend.converters.PatientConverter;
+import org.tsa.hms_backend.dtos.PasswordChangeDto;
 import org.tsa.hms_backend.dtos.PatientsDto;
+import org.tsa.hms_backend.entities.Doctors;
 import org.tsa.hms_backend.entities.Patients;
 import org.tsa.hms_backend.repositories.PatientRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +26,41 @@ public class PatientService {
         return null;
     }
 
+    public Patients findById(Long id) {
+        return patientRepository.findById(id).orElse(null);
+    }
 
+    public Patients update(PatientsDto patientsDto, Long id) {
+        Patients existingPatient = patientRepository.findById(id).orElse(null);
+        if (existingPatient == null) {
+            return null;
+        }
+        Patients updatingPatient = prepareForUpdate(existingPatient, patientsDto);
+        return patientRepository.save(updatingPatient);
+    }
+
+    public Patients prepareForUpdate(Patients patient, PatientsDto patientsDto) {
+        patient.setAddress(patientsDto.getAddress());
+        patient.setBloodGroup(patientsDto.getBloodGroup());
+        Long userId = patient.getUser().getId();
+        patient.setUser(patientsDto.getUser());
+        patient.getUser().setId(userId);
+        return patient;
+    }
+
+    public Boolean changePassword(PasswordChangeDto passwordChangeDto, Long id) {
+        Patients existingPatient = patientRepository.findById(id).orElse(null);
+        if (existingPatient == null) {
+            return false;
+        }
+        if (existingPatient.getUser().getEmail().equals(passwordChangeDto.getUsername())&&existingPatient.getUser().getPassword().equals(passwordChangeDto.getOldPassword())) {
+            existingPatient.getUser().setPassword(passwordChangeDto.getNewPassword());
+            return true;
+        }
+        return false;
+    }
+
+    public List<Doctors> getDoctors() {
+        return List.of();
+    }
 }
